a4c95825bfcb5eb705e2b041ecd53dea025421e6
mon-ina <inakamonn0909@gmail.com>
2025-10-24 11:41:58 +0900

    ログイン機能ができた

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,26 +3,28 @@ class SessionsController < ApplicationController
   end
 
   def create
+    # 教員ログインのみ
     user = User.find_by(account_name: params[:account_name])
-
     if user&.authenticate(params[:password])
       session[:user_id] = user.id
-      redirect_to timetables_path, notice: "ログインしました"
+      session[:role] = user.role
+      redirect_to timetables_path
     else
-      flash.now[:alert] = "アカウント名またはパスワードが違います"
-      render :new, status: :unprocessable_entity
+      flash.now[:alert] = "アカウント名またはパスワードが間違っています"
+      render :new
     end
   end
 
   def guest_login
-    # ゲスト（生徒）扱いとして session にroleを保存
-    session[:role] = "student"
-    redirect_to timetables_path, notice: "生徒としてログインしました"
+    # 生徒用ゲストログイン
+    session[:user_id] = nil
+    session[:role] = 'student'
+    redirect_to timetables_path
   end
 
   def destroy
-    session.delete(:user_id)
-    session.delete(:role)
-    redirect_to login_path, notice: "ログアウトしました"
+    session[:user_id] = nil
+    session[:role] = nil
+    redirect_to login_path
   end
 end
