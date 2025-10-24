65e2912b0ee8b168f4a6a437c943f8b3ef8d272f
mon-ina <inakamonn0909@gmail.com>
2025-10-24 12:57:57 +0900

    来週先週無限現象がなおった

--- app/controllers/timetables_controller.rb
+++ app/controllers/timetables_controller.rb
@@ -1,16 +1,23 @@
 class TimetablesController < ApplicationController
-  #before_action :require_login
+  before_action :require_login
 
   def index
     @grade = params[:grade].present? ? params[:grade].to_i : 1
 
     today = Date.today
     week_offset = params[:week_offset].to_i || 0
+
+    # week_offset を -1〜1 の範囲に制限
+    week_offset = -1 if week_offset < -1
+    week_offset = 1 if week_offset > 1
+
     @week_start = today.beginning_of_week + (week_offset * 7)
 
     @timetables = Timetable.includes(:subject)
                            .where(week_start_date: @week_start, grade: @grade)
                            .order(:day_of_week, :period)
+
+    @week_offset = week_offset # ビューでリンク生成に使用
   end
 
   private
